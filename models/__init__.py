5954e769de0d2c8f1abbe26a1e6bbb64f9ff4754
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-08-13 13:38:00 +0530

    Fixed init file for importing other files

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-import student_resnet, student_resnet_wide
-import teacher_resnet, teacher_resnet_wide
+from . import student_resnet, student_resnet_wide
+from . import teacher_resnet, teacher_resnet_wide
